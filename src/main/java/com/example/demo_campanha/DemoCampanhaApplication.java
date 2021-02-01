c7409a42ebf26bfac057903409af5037150328bb
João Paulo Nóbrega Figueiredo <joaopnobrega@gmail.com>
2021-02-01 12:02:58 -0300

    Project created

---
+++ src/main/java/com/example/demo_campanha/DemoCampanhaApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo_campanha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCampanhaApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DemoCampanhaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo_campanha/DemoCampanhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_campanha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCampanhaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
